a8d704f0e088b097ad964f5411eaf7a43ed04439
gabides <gdittrick@gmail.com>
2018-04-17 16:06:19 +0200

    Initial Commit

---
+++ LTM-Engine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LTM-Engine
+//
+//  Created by Gabriel Dittrick on 17/04/2018.
+//  Copyright © 2018 Gabriel Dittrick. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
