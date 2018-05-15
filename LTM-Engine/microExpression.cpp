9ccb54c398e72a42af3b62765d43ad8d01bf76b1
gabides <gdittrick@gmail.com>
2018-05-15 16:00:32 +0200

    created class microExpression

---
+++ LTM-Engine/microExpression.cpp
@@ -0,0 +1,25 @@
+//
+//  microExpression.cpp
+//  LTM-Engine
+//
+//  Created by Gabriel Dittrick on 15/05/2018.
+//  Copyright © 2018 Gabriel Dittrick. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+//#include <fstream>
+
+using namespace std;
+class MicroExpression
+{
+public:
+    int AU;
+    int onset;
+    int offset;
+    string emotion;
+    
+    
+    
+};
+
